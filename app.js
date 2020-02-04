76e512eb7017e4891e0d8033cc6b92cbae504a7d
Lu-Ma-Ne <luguneves@gmail.com>
2020-02-04 15:06:13 +0000

    commit

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ console.log(firstName, favouriteColor);
 
 var x = 'Laurel';
 console.log(x);
+var x = 'Laurel';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+console.log(y);
+var favouriteFood = 'sea fruits';
+var city = 'Dublin';
+var country = 'Ireland';
+// this command writes the message that is between () to the console. 
+console. log(firstName, favouriteColor, favouriteFood);
+console.log(city, country);
+
+var phrase, endingPhrase, one, two, three, four, five, six, seven;
+
+phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+endingPhrase = " is what we see. Rainbow colours, are made with light bending you see!";
+one = "Red";
+two = "orange";
+three = "yellow";
+four = "green";
+five = "blue";
+six = "indigo";
+seven = "violet";
+
+console.log(phrase, one, two, three, four, five, six, seven, endingPhrase);
+
+
